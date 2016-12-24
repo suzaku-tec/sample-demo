6d5b9c7e6db44f24de676241f6dc1b18642ec44f
ikarashi <ikarashi@192.168.2.100>
2016-12-24 22:31:48 +0900

    update insert sql

--- demo/src/main/java/com/example/HelloController.java
+++ demo/src/main/java/com/example/HelloController.java
@@ -5,6 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import jp.sf.amateras.mirage.ClasspathSqlResource;
+import jp.sf.amateras.mirage.SqlManager;
+import jp.sf.amateras.mirage.SqlResource;
+import jp.sf.amateras.mirage.session.Session;
+import jp.sf.amateras.mirage.session.SessionFactory;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
@@ -19,5 +25,25 @@ public class HelloController {
         hoge.value = "Response";
 
         return hoge;
+    }
+    
+    @RequestMapping("/insert")
+    public String insert() {
+    	
+    	Session session = SessionFactory.getSession();
+    	SqlManager sqlManager = session.getSqlManager();
+    	
+    	SqlResource selectUserSql = new ClasspathSqlResource("insert.sql");
+    	
+    	session.begin();
+    	
+    	Sample s = new Sample();
+    	s.setName("aaaaaaa");
+    	
+    	sqlManager.executeUpdate(selectUserSql, s);
+    	
+    	session.commit();
+
+    	return "aaaaaaaaaaaaa";
     }
 }
---
+++ demo/src/main/resources/insert.sql
@@ -0,0 +1,4 @@
+insert into sample(name)
+values (
+	/*name*/'test'
+)
